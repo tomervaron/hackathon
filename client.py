f59cec0551d677e8683ed282fbc16bd70d25e25f
tomerkel <73695609+tomerkel@users.noreply.github.com>
2020-12-30 12:56:13 +0200

    sda

--- client.py
+++ client.py
@@ -2,9 +2,9 @@ import socket
 import struct
 import time
 from threading import Thread
-# from curtsies import Input
+from curtsies import Input
 # import getch
-from pynput import keyboard
+
 
 MAGIC_COOKIE = hex(0xfeedbeef)
 MESSAGE_TYPE = hex(0x2)
@@ -45,23 +45,18 @@ while True:
         print(msg.decode("utf-8"))
         end_time = time.time() + 10
 
-        # with Input(keynames='curtsies') as input_generator:
-        #     for e in Input():
-        #         if time.time() >= end_time:
-        #             break
-        #         else:
-        #             on_press(e)
+        with Input(keynames='curtsies') as input_generator:
+            input_generator._wait_for_read_ready_or_timeout(10)
+            for e in Input():
+                if time.time() >= end_time:
+                    break
+                else:
+                    on_press(e)
+
+                   
         # while time.time()<end_time:
         #     c = getch.getch()
         #     on_press(c)
-
-        with keyboard.Listener(on_press=on_press, suppress=True) as listener:
-            def time_out(time_to_run: int):
-                time.sleep(time_to_run)
-                listener.stop()
-               
-            Thread(target=time_out, args=(10,), ).start()
-            listener.join()
 
         break
 client_socket_tcp.close()
